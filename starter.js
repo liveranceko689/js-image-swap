c274142acb1825335881913b57639344b9eb15a5
liveranceko689 <Liveranceko689@northwested.org>
2026-04-29 14:15:29 -0400

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,1 +1,23 @@
+// Kohen Liverance
+// 4/29/26
+// JS Image Swap Project
 
+
+const modelImages = ["cut-out.jpg, background-removal - Copy.png"]
+
+
+const imgElement = document.getElementById("model-frame");
+
+
+const preload = new Image();
+preload.src = modelImages[1];
+
+
+imgElement.addEventListener('mouseenter', () => {
+    imgElement.src = modelImages[1];
+});
+
+
+imgElement.addEventListener('mouseleave', () => {
+    imgElement.src = modelImages[0];
+});
